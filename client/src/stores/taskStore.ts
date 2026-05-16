641b5e8ca24a930b15aa6ef39ff8d15cc58810e5
edkiada <yyh901277111@gmail.com>
2026-05-16 17:44:50 +0800

    feat: API logic(post) in pinia

--- client/src/stores/taskStore.ts
+++ client/src/stores/taskStore.ts
@@ -12,19 +12,35 @@ interface Task {
   note: string[];
 }
 
+interface NewTaskInput {
+  title: string;
+  priority: 'low' | 'medium' | 'high';
+}
+
+const API_URL = 'http://localhost:3001/api/tasks';
+
 export const useTaskStore = defineStore('taskStore', {
   state: () => ({
     tasks: [] as Task[],
+    newTask: [] as Task[]
   }),
   actions: {
     async fetchTasks() {
       try {
         // Point this to your actual backend URL
-        const response = await axios.get('http://localhost:3001/api/tasks');
+        const response = await axios.get(API_URL);
         this.tasks = response.data;
       } catch (error) {
         console.error("Connection failed:", error);
       }
-    }
+    },
+    async saveTask(task: NewTaskInput) {
+      try {
+        const response = await axios.post(API_URL, task);
+        this.tasks.push(response.data); // Add the new task to the local state
+      } catch (error) {
+        console.error("Failed to save task:", error);
+      }
+    }   
   }
 });
