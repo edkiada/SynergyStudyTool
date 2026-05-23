ba240afb396087594c216d05145892618ef743b7
edkiada <yyh901277111@gmail.com>
2026-05-23 19:47:56 +0800

    fix: fix cors parameter

--- server/src/app.js
+++ server/src/app.js
@@ -12,7 +12,11 @@ db.connectDB()
 
 app.use(express.json())
 app.use(middleware.requestLogger)
-app.use(cors())
+app.use(cors({
+  origin: '*', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
 
 app.use('/api/tasks', tasksRouter)
 app.use('/api/notes', notesRouter)
