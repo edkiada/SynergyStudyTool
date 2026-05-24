8aa03f55dc1910c514e4f1bea4518c8b0cd11dcd
edkiada <yyh901277111@gmail.com>
2026-05-24 22:50:25 +0800

    feat: task logic(sort task)

---
+++ server/src/services/taskService.js
@@ -0,0 +1,28 @@
+class TaskEngineService {
+
+  static sortTasksByCustom(tasks) {
+    const priorityWeights = {
+      'high': 3,
+      'medium': 2,
+      'low': 1
+    }
+
+    const sortedResults = [...tasks].sort((a, b) => {
+      const priorityA = priorityWeights[a.priority] || 0;
+      const priorityB = priorityWeights[b.priority] || 0;
+
+      const iscompletedA = a.status === 'completed' ? 1 : 0;
+      const iscompletedB = b.status === 'completed' ? 1 : 0;
+
+      if (iscompletedA !== iscompletedB) {
+        return iscompletedA - iscompletedB; 
+      }
+      else {
+        return priorityB - priorityA;
+      }
+    })
+    return sortedResults;
+  }
+}
+
+module.exports = TaskEngineService;
