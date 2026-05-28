8af7c232a781f190cc145c8301c4b9f1a358f7a9
edkiada <yyh901277111@gmail.com>
2026-05-28 16:13:02 +0800

    feat(view): add focus view

---
+++ client/src/view/focusView.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+  import headerBar from '../components/header/header.vue'
+  import navBer from '../components/navBar/navBar.vue'
+
+</script>
+
+<template>
+  <headerBar />
+  <main>
+    <navBer />
+  </main>
+</template>
+
+<style scoped></style>
