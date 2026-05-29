1985a4136f19c9312ed4e4ad194f0dfebeb0e483
edkiada <yyh901277111@gmail.com>
2026-05-29 17:31:58 +0800

    fix(view): reset viewPage variable bug

--- client/src/view/focusView.vue
+++ client/src/view/focusView.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-  import { onMounted } from 'vue';
-  import { useMainStore } from '../stores/mainStore';
+  import { onMounted } from 'vue'
+  import { useMainStore } from '../stores/mainStore'
   import headerBar from '../components/globalComponents/header/header.vue'
   import navBer from '../components/globalComponents/navBar/navBar.vue'
   import dualTimer from '../components/focusComponents/dualTimer/dualTimer.vue'
