1f65e910687f379b952695b2b699f58a64e000a5
edkiada <yyh901277111@gmail.com>
2026-05-29 17:28:49 +0800

    feat: timer UI design

---
+++ client/src/components/focusComponents/dualTimer/dualTimer.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <section class="timerContainer">
+    <div class="timerItem timerMode">
+      <button class="timerBtn modeBtn leftBtn"><span>FocusBlock</span></button>
+      <button class="timerBtn modeBtn rightBtn"><span>TimeTracker</span></button>
+    </div>
+    <div class="timerItem timerDisplay">
+      <p class="timeTextHour">1H</p>
+      <p class="timerTextMain">25 : 00</p>
+    </div>
+    <div class="timerItem timerControl">
+      <button class="timerBtn controlBtn"><span>START</span></button>
+      <button class="timerBtn controlBtn"><span>STOP</span></button>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+  @import './dualTimer.css';
+</style>
--- client/src/view/focusView.vue
+++ client/src/view/focusView.vue
@@ -1,13 +1,21 @@
 <script setup lang="ts">
+  import { onMounted } from 'vue';
+  import { useMainStore } from '../stores/mainStore';
   import headerBar from '../components/globalComponents/header/header.vue'
   import navBer from '../components/globalComponents/navBar/navBar.vue'
+  import dualTimer from '../components/focusComponents/dualTimer/dualTimer.vue'
+  const mainStore = useMainStore()
 
+  onMounted(() => {
+    mainStore.nowView = 'focus'
+  })
 </script>
 
 <template>
   <headerBar />
   <main>
     <navBer />
+    <dualTimer />
   </main>
 </template>
 
