170f12439b21c43873fc08d274435898c644e0c4
edkiada <yyh901277111@gmail.com>
2026-06-03 22:31:39 +0800

    feat: initial focusStore

---
+++ client/src/stores/focusStore.ts
@@ -0,0 +1,30 @@
+import axios from 'axios';
+import { defineStore } from 'pinia';
+
+interface addFocus {
+  startTime: string;
+  duration: number;
+  source: {
+    type: string;
+    refId?: string;
+    onModel?: string;
+  }
+}
+
+const API_URL = `http://192.168.0.113:3001/api/focusSession`
+
+export const useFocusStore = defineStore('focusStore', {
+  state: () => ({
+    focusSessions: [] as addFocus[],
+  }),
+  actions: {
+    async saveFocusSessions(focus: addFocus) {
+      try {
+        const res = await axios.post(API_URL, focus);
+        console.log('Focus session saved:', res.data);
+      } catch(error) {
+        console.error('Failed to save focus session:', error);
+      }
+    }
+  }
+})
