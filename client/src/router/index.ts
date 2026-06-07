161f3ec3d82ac36e336780358f65a54adb75b224
edkiada <yyh901277111@gmail.com>
2026-06-07 15:44:37 +0800

    feat: initial calendar view

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import taskView from '../view/taskView.vue'
 import focusView from '../view/focusView.vue'
+import calendarView from '../view/calendarView.vue';
 
 const routes = [
   {
@@ -12,6 +13,11 @@ const routes = [
     path: '/focus',
     name: 'focus',
     component: focusView
+  },
+  {
+    path: '/calendar',
+    name: 'calendar',
+    component: calendarView
   }
 ];
 
---
+++ client/src/view/calendarView.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+  import { onMounted } from 'vue';
+  import { useMainStore } from '../stores/mainStore';
+  import cHeader from '../components/calendarComponents/cHeader/cHeader.vue';
+  import NavBar from '../components/globalComponents/navBar/navBar.vue';
+  const mainStore = useMainStore();
+
+  onMounted(() => {
+    mainStore.nowView = 'calendar';
+  });
+</script>
+
+<template>
+  <cHeader />
+  <main>
+    <NavBar />
+  </main>
+</template>
+
+<style scoped></style>
