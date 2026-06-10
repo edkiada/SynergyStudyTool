fea4f181c842f849493a914a6fb216042f0e3dbc
edkiada <yyh901277111@gmail.com>
2026-06-10 22:40:53 +0800

    feat: initial timeLine components

---
+++ client/src/components/calendarComponents/timeLine/timeLine.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <section>
+    <div class="timeContainer">
+      <div v-for="time in 18" :key="time" class="timeItem">
+        <p class="timeText">{{ time + 5 }} : 00</p>
+        <div class="test"></div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+  @import './timeLine.css'
+</style>
--- client/src/view/calendarView.vue
+++ client/src/view/calendarView.vue
@@ -3,6 +3,7 @@
   import { useMainStore } from '../stores/mainStore';
   import cHeader from '../components/calendarComponents/cHeader/cHeader.vue';
   import NavBar from '../components/globalComponents/navBar/navBar.vue';
+  import timeLine from '../components/calendarComponents/timeLine/timeLine.vue';
   const mainStore = useMainStore();
 
   onMounted(() => {
@@ -14,6 +15,7 @@
   <cHeader />
   <main>
     <NavBar />
+    <timeLine />
   </main>
 </template>
 
