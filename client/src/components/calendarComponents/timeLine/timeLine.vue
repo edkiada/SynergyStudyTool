bba9dabe6058c88e98f8d7ac0a177c5aed8f2485
edkiada <yyh901277111@gmail.com>
2026-06-11 17:37:06 +0800

    feat: focusSessions Visualization

--- client/src/components/calendarComponents/timeLine/timeLine.vue
+++ client/src/components/calendarComponents/timeLine/timeLine.vue
@@ -1,4 +1,33 @@
 <script setup lang="ts">
+  import { useFocusStore } from '../../../stores/focusStore';
+  import { computed, onMounted } from 'vue';
+  const focusStore = useFocusStore()
+
+  const formatFocusSessions = computed(() => {
+    const sessions = focusStore.calendarFocus;
+    const HOUR_HEIGHT = 80;
+    const START_HOUR = 6;
+    
+    return sessions.map(session => {
+      const startTime = new Date(session.startTime);
+      const startHour = startTime.getHours();
+      const startMinute = startTime.getMinutes();
+
+      const minutesFromStart = (startHour - START_HOUR) * 60 + startMinute;
+      const topPosition = (minutesFromStart / 60) * HOUR_HEIGHT;
+      const blockHeight = (session.duration / 3600) * HOUR_HEIGHT;
+
+      return {
+        ...session,
+        top: `${topPosition}px`,
+        height: `${blockHeight}px`
+      }
+    })
+  })
+
+  onMounted(() => {
+    focusStore.fetchFocusSessions()
+  })
 
 </script>
 
@@ -8,6 +37,11 @@
       <div v-for="time in 18" :key="time" class="timeItem">
         <p class="timeText">{{ time + 5 }} : 00</p>
         <div class="test"></div>
+      </div>
+      <div class="timeLineContent">
+        <div v-for="session in formatFocusSessions" :key="session.id" class="sessionBlock" :style="{'top': session.top, 'height': session.height}">
+          <p class="sessionText">test</p>
+        </div>
       </div>
     </div>
   </section>
--- client/src/stores/focusStore.ts
+++ client/src/stores/focusStore.ts
@@ -1,6 +1,17 @@
 import axios from 'axios';
 import { defineStore } from 'pinia';
 
+interface FocusSession {
+  id: string;
+  startTime: Date;
+  duration: number;
+  source: {
+    type: string;
+    refId?: string;
+    onModel?: string;
+  }
+}
+
 interface addFocus {
   startTime: Date;
   duration: number;
@@ -16,6 +27,7 @@ const API_URL = `http://192.168.0.113:3001/api/focusSession`
 export const useFocusStore = defineStore('focusStore', {
   state: () => ({
     focusSessions: [] as addFocus[],
+    calendarFocus: [] as FocusSession[]
   }),
   actions: {
     async saveFocusSessions(focus: addFocus) {
@@ -24,6 +36,20 @@ export const useFocusStore = defineStore('focusStore', {
         console.log('Focus session saved:', res.data);
       } catch(error) {
         console.error('Failed to save focus session:', error);
+      }
+    },
+    async fetchFocusSessions() {
+      try {
+        const res = await axios.get(`${API_URL}/calendar`, {
+          params: {
+            year: 2026,
+            month: 5,
+            day: 11
+          }
+        });
+        this.calendarFocus = res.data;
+      } catch(error) {
+        console.error('Failed to fetch focus sessions:', error);
       }
     }
   }
