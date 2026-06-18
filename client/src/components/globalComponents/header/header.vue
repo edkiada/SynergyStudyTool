8d3ebdf6f1e3c736b3c7208ddfe72459c47885fa
edkiada <yyh901277111@gmail.com>
2026-06-18 21:03:46 +0800

    feat(client): user Info page(overlay mode)

--- client/src/components/globalComponents/header/header.vue
+++ client/src/components/globalComponents/header/header.vue
@@ -1,13 +1,19 @@
 <script setup lang="ts">
-  import { computed } from 'vue';
+  import { computed, ref } from 'vue';
   import userIcon from '../../../assets/icons/user.svg'
+  import userInfo from '../userInfo/userInfo.vue'
   const today = new Date()
+  const userInfoOpen = ref(false);
   const updateDate = computed(() => {
     return today.toLocaleDateString('en-MG', {
       day: 'numeric',
       month: 'short'
     })
   })
+
+  const openUserInfo = () => {
+    userInfoOpen.value = true;
+  }
 </script>
 
 <template>
@@ -16,7 +22,8 @@
       <h1>Today</h1>
       <h2>{{ updateDate }}</h2>
     </div>
-    <img :src="userIcon" alt="userIcon" class="userIcon">
+    <button class="infoBtn" type="button" @click="openUserInfo()"><img :src="userIcon" alt="userIcon" class="userIcon"></button>
+    <userInfo v-model:open="userInfoOpen" v-if="userInfoOpen"/>
   </header>
 </template>
 
---
+++ client/src/components/globalComponents/userInfo/userInfo.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+  import closeIcon from '../../../assets/icons/close.svg'
+  import userIcon from '../../../assets/icons/user.svg'
+  import loginIcon from '../../../assets/icons/login.svg'
+  import leftArrow from '../../../assets/icons/leftArrow.svg'
+  import { ref } from 'vue'
+  defineProps({
+    open: Boolean
+  })
+
+  const emit = defineEmits(['update:open'])
+  const ischildPageOpen = ref(false)
+
+  const closeOverlay = () => {
+    emit('update:open', false)
+  }
+
+  const toggleChildPage = () => {
+    ischildPageOpen.value = !ischildPageOpen.value;
+  }
+</script>
+
+<template>
+  <section class="userOverlay">
+    <div class="closeContent">
+      <button class="closeBtn" type="button" @click="closeOverlay"><img :src="closeIcon" class="closeIcon" alt="closeIcon"></button>
+    </div>
+    <div class="userContent">
+      <img :src="userIcon" alt="userIcon" class="userIcon"/>
+      <p>User Name</p>
+    </div>
+    <div class="loginContent">
+      <button class="loginBtn" type="button" @click="toggleChildPage">
+        <img :src="loginIcon" alt="loginIcon" class="loginIcon">
+        <p class="loginText">Login</p>
+      </button>
+    </div>
+  </section>
+  <section class="userOverlay " v-if="ischildPageOpen">
+    <div class="closeContent">
+      <button class="closeBtn" type="button" @click="toggleChildPage">
+        <img :src="leftArrow" class="leftArrow" alt="leftArrow">
+        <p>Info</p>
+      </button>
+    </div>
+  </section>
+  <div class="overlayBG"></div>
+</template>
+
+<style scoped>
+  @import './userInfo.css';
+</style>
