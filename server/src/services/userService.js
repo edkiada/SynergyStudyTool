30cb9e12bf4e22970406cdb79a76d80b26a693d2
edkiada <yyh901277111@gmail.com>
2026-06-21 17:16:17 +0800

    fix(server): wrong format of getTokenFrom function and forgot next()

--- server/src/services/userService.js
+++ server/src/services/userService.js
@@ -3,21 +3,32 @@ const User = require('../models/user.js')
 
 const verifyToken = async (token) => {
   if(!token) {
-    throw new Error('TokenMissing')
+    const error = new Error('TokenMissing')
+    error.name = 'TokenMissing'
+    throw error
   }
   try {
     const decodedToken = jwt.verify(token, process.env.SECRET)
     if(!decodedToken.id) {
-      throw new Error('TokenInvalid')
+      const error = new Error('TokenInvalid')
+      error.name = 'TokenInvalid'
+      throw error
     }
 
     const user = await User.findById(decodedToken.id)
     if(!user) {
-      throw new Error('userNotFind')
+      const error = new Error('userNotFind')
+      error.name = 'userNotFind'
+      throw error
     }
     return user
 
   } catch(error) {
+    if(error.name === 'JsonWebTokenError' || error.name === 'TokenExpiredError') {
+      const tokenError = new Error('TokenInvalid')
+      tokenError.name = 'TokenInvalid'
+      throw tokenError
+    }
     throw error
   }
 }
