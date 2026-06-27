0620e8369aa10dfdcd7e086c6fd6fdbcf3184fd7
edkiada <yyh901277111@gmail.com>
2026-06-27 16:59:18 +0800

    ftx(server): lost

--- server/src/routes/TasksRouter.js
+++ server/src/routes/TasksRouter.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 const tasksControllers = require('../controllers/tasks')
 const middle = require('../middleware')
 
-router.get('/', tasksControllers.getAllTasks)
+router.get('/', middle.userExtractor, tasksControllers.getAllTasks)
 router.post('/', middle.userExtractor, tasksControllers.createTask)
 router.put('/:id', tasksControllers.updateTask)
 router.delete('/:id', tasksControllers.deleteTask)
