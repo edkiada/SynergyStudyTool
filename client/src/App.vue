4f4d33c676fe2b2a5de33aa54911ca5ecde7570a
edkiada <yyh901277111@gmail.com>
2026-07-06 20:41:04 +0800

    feat(client): RWD design on desktop

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,6 +1,15 @@
 <script setup lang="ts">
+  import { useBreakpoints, breakpointsTailwind } from '@vueuse/core';
+  import mainView from './view/desktopView.vue'
+  const breakpoints = useBreakpoints(breakpointsTailwind);
+  const isDesktop = breakpoints.greaterOrEqual('md')
 </script>
 
 <template>
-  <RouterView />
+  <div v-if="isDesktop">
+    <mainView />
+  </div>
+  <div v-else>
+    <RouterView />
+  </div>
 </template>
