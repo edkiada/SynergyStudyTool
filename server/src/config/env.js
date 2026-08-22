519683be4d7ac55b354c8a3ac7d297e5de00275a
edkiada <yyh901277111@gmail.com>
2026-08-22 16:51:23 +0800

    feat(server): module change

--- server/src/config/env.js
+++ server/src/config/env.js
@@ -6,10 +6,14 @@ const MONGODB_URI = process.env.NODE_ENV === 'prod'
   ? process.env.PROD_MONGODB_URI
   : process.env.MONGODB_URI
 
+
+const CLOUDFLARE_ACCOUNT_ID = process.env.CLOUDFLARE_ACCOUNT_ID
 const GEMINI_API_KEY = process.env.GEMINI_API_KEY
 
 module.exports = {
   PORT,
   MONGODB_URI,
-  GEMINI_API_KEY
+  GEMINI_API_KEY,
+  CLOUDFLARE_ACCOUNT_ID,
+  CLOUDFLARE_API_TOKEN: process.env.CLOUDFLARE_API_TOKEN,
 }
